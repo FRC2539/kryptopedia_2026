48244da266fa8375ea879fee36f33671e8e5fa14
Dominic <dominic@userexe.me>
2026-04-20 21:29:32 -0400

    proper 404s for scouting data items

--- kryptopedia_dashboard/app/controllers/scouting_data_items_controller.rb
+++ kryptopedia_dashboard/app/controllers/scouting_data_items_controller.rb
@@ -7,11 +7,11 @@ class ScoutingDataItemsController < ApplicationController
   end
 
   def edit
-    @item = @scouted_event.scouting_data_items.find_by(uid: params[:uid])
+    @item = @scouted_event.scouting_data_items.find_by!(uid: params[:uid])
   end
 
   def update
-    @item = @scouted_event.scouting_data_items.find_by(uid: params[:uid])
+    @item = @scouted_event.scouting_data_items.find_by!(uid: params[:uid])
     data = params[:scouting_data_item][:data]
     parsed_data = JSON.parse(data) rescue nil
     if parsed_data && @item.update(data: parsed_data)
@@ -22,13 +22,13 @@ class ScoutingDataItemsController < ApplicationController
   end
 
   def destroy
-    @item = @scouted_event.scouting_data_items.find_by(uid: params[:uid])
+    @item = @scouted_event.scouting_data_items.find_by!(uid: params[:uid])
     @item.destroy
     redirect_to team_scouted_event_scouting_data_items_path(@scouted_event), notice: "Item deleted!"
   end
 
   def restore
-    @item = @scouted_event.scouting_data_items.find_by(uid: params[:scouting_data_item_uid])
+    @item = @scouted_event.scouting_data_items.find_by!(uid: params[:scouting_data_item_uid])
     if @item.deleted?
       @item.update(deleted_at: nil)
       redirect_to team_scouted_event_scouting_data_items_path(@scouted_event), notice: "Item restored!"
